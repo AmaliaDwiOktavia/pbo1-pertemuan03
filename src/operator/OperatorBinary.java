55bf3cab90d193a1d74be46a1a0f180ec73e5fc6
AmaliaDwiOktavia <amaliadwioktavia@gmail.com>
2021-04-23 10:55:34 +0800

    Operator binary assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Amalia Dwi Oktavia";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.87;
+
+}
