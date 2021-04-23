4a8417071b91c33e9d474ed11cbc3aaba85991bc
AmaliaDwiOktavia <amaliadwioktavia@gmail.com>
2021-04-23 11:00:32 +0800

    percabangan ifelse condition false sehingga statement1

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -7,6 +7,9 @@ public class IfElse {
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang Cukup, angsul : "+angsul);
         }else{
             System.out.println("Uang cukup... selamat menikmati..");
         }
